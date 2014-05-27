78c1b3357557a8f10e81a49df0fdfe1886e5c868
Guido Zockoll <gzockoll@gmail.com>
2014-05-27 15:54:09 +0200

    timing, volumes-from

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ if [ $COUNT -eq 0 ]; then
   docker run -v /var/lib/mysql --name sonar-data busybox true
 fi
 
-docker run -d -p 33306:3306 --name sonar-db -e MYSQL_PASS=123456 gzockoll/mysql-5.6
+docker run -d -p 33306:3306 --name sonar-db --volumes-from sonar-data -e MYSQL_PASS=123456 payone/mysql-5.6
 docker run -d -p 9000:9000 --name sonar-master --link sonar-db:db_1 sonar
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Wating for database to come up ..."
-sleep 10
+sleep 15
 echo "CREATE DATABASE IF NOT EXISTS sonar;" | mysql -u admin -h db_1 -p123456
 while true; do
 /opt/sonarqube/bin/linux-x86-64/sonar.sh console
