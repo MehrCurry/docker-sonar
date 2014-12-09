6a09c2c852ff1ab58507ef08cc066f04d10e1951
Guido Zockoll <Guido.Zockoll@payone.de>
2014-12-09 07:43:11 +0100

    Deprecated plugins removed Update all plugin versions added --restart=always to run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ if [ $COUNT -eq 0 ]; then
   docker run -v /var/lib/mysql --name sonar-data busybox true
 fi
 
-docker run -d -p 33306:3306 --name sonar-db --volumes-from sonar-data -e MYSQL_PASS=123456 payone/mysql-5.6
-docker run -d -p 9000:9000 --name sonar-master --link sonar-db:db_1 sonar
+docker run -d -p 33306:3306 --name sonar-db --restart=always --volumes-from sonar-data -e MYSQL_PASS=123456 payone/mysql-5.6
+docker run -d -p 9000:9000 --name sonar-master --restart=always --link sonar-db:db_1 sonar
