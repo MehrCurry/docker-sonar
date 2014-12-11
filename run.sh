35af42fc802ec501838d685b3c47477dea5d28a7
Guido Zockoll <gzockoll@gmail.com>
2014-12-11 13:40:52 +0100

    better run command

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@
 #Setup Data Container
 COUNT=$(docker ps -a |grep sonar-data |wc -l)
 if [ $COUNT -eq 0 ]; then
+  # Create an not running data only cotainer
   docker run -v /var/lib/mysql --name sonar-data busybox true
 fi
 
-docker run -d -p 33306:3306 --name sonar-db --volumes-from sonar-data -e MYSQL_PASS=123456 payone/mysql-5.6
+COUNT=$(docker ps -a |grep sonar-db |wc -l)
+if [ $COUNT -eq 0 ]; then
+  docker run -d -p 33306:3306 --name sonar-db --volumes-from sonar-data -e MYSQL_PASS=123456 payone/mysql-5.6
+fi
+
 docker run -d -p 9000:9000 --name sonar-master --link sonar-db:db_1 sonar
