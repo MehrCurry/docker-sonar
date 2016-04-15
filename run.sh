2eba43a58767323e8893f1337d4c5bbfe871cc1b
Guido Zockoll <gzockoll@gmail.com>
2016-04-15 09:12:42 +0200

    Revert "bla"
    This reverts commit 5607d59922ddcd7d770c51407a817f5998aacfab.

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+#Setup Data Container
+DATA=$(docker ps -a |grep sonar-data | awk '{print $1}')
+if [ -z "$DATA" ]; then
+  docker run -v /var/lib/mysql --name sonar-data busybox true
+fi
+
+docker rm -f sonar-db ||:
 docker rm -f sonar-master ||:
-docker run -d -p 9000:9000 --name sonar-master --restart=always -v $(pwd)/sonar.properties:/opt/sonarqube/conf/sonar.properties gzockoll/sonar
+
+docker run -d -p 33306:3306 --name sonar-db --restart=always --volumes-from sonar-data -e MYSQL_PASS=123456 tutum/mysql:5.6
+docker run -d -p 9000:9000 --name sonar-master --restart=always --link sonar-db:db gzockoll/sonar
